4f82f00b16de81eaa344a34f3b2997b10f9f1581
dafariski77 <rskdafa@gmail.com>
2023-06-16 21:28:58 +0700

    delete organizer_id

--- database/migrations/2023_06_13_142931_create_categories_table.php
+++ database/migrations/2023_06_13_142931_create_categories_table.php
@@ -13,10 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('organizer_id');
             $table->string("name");
             $table->string("image");
-            // $table->foreign('organizer_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
